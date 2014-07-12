9550a979a80bde219bc82698dfa54a9e85387022
pieflicker <nesbitt.greg@gmail.com>
2014-07-12 19:14:34 -0400

    first commit, mostly html/css so far

---
+++ app/controllers/fizzbuzz.rb
@@ -0,0 +1,14 @@
+
+
+(1..100).each do |x|
+  if x%3==0 && x%5==0
+    puts "fizzbuzz"
+  elsif x%3==0 
+    puts "fizz"
+  elsif x%5==0
+    puts "buzz"
+  else 
+    puts "#{x}"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopframe::Application.config.secret_key_base = '6aec21c36c67371a10b2012909f52b283bd60d81765061e7500ca445f403a3894806c973fc7577f0b2032aea8787cbe268159ec3e4817d4a14570dfa205883a6'
